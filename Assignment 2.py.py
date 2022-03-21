b1e615eab0af81866336292f8937ab1680144fa9
patilkomal19 <komalu19@gmail.com>
2022-03-21 14:51:35 +0530

    added gitignore

---
+++ Assignment 2.py.py
@@ -0,0 +1,63 @@
+#Display the no. which are divisible by 5
+
+#for i in range (20,45): 
+#     if (i%5 == 0):
+#        print(i)
+
+#Take your name as a string & print character with its index value
+
+#x = "komal patil"
+#i = 0
+#for element in x:
+#    print(f"the character is {x[i]} and index is{i}")
+#    i = i+1
+
+#Display the no of the range in decreasing order
+
+#for i in range (40,15,-2):
+    #print(i)
+
+#Take the list you need to multiply all the element
+
+#x = eval(input("enter the list:"))
+#mul = 1
+#for i in x:
+   # mul = mul*i
+#    print(mul)
+    
+##Take the set you need to multiply all the element
+
+#x = eval(input("enter the set:"))
+#mul = 1
+#for i in x:
+#    mul = mul*i
+#    print(mul)
+
+
+#x = eval(input("enter the list:"))
+#sum = 0
+#for i in x:
+#    sum = sum + i**2
+#print(f"the sum of the sqr of liSt is {sum}")
+
+#for loop
+
+##n = "welcome to python"
+#i = 0
+#for element in n:
+#    print(f"the character is {element} and the index is{i}")
+#    i = i+1
+
+#while loop
+
+#n = "welcome to python"
+#i = 0
+#while i < len(n):
+#    print(n[i],end="")
+#    i =i+1
+
+n = eval(input("enter the list"))       # [10,20,30,40,50]
+i = 0
+while i < len(n):
+    print(f"the number is {n[i]}and the index is {i}")
+    i += 1
